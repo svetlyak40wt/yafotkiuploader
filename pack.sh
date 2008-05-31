56ac9ed335ee56ac2ae9364766d8c5e71c031ed5
Alexander Artemenko <art@yandex-team.ru>
2008-05-31 19:57:25 +0400

    pack.sh was added.

---
+++ pack.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+VERSION=$(grep '^VERSION' YaFotkiUploader.py | sed "s/VERSION = '\(.*\)'/\1/")
+
+cd ..
+
+tar -jcf YaFotkiUploader-$VERSION.tar.bz2 YaFotkiUploader --exclude .git --exclude '*.swp' --exclude '*.pyc'
