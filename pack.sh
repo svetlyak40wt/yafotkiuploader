fb793e8181164f38c133d7dd418ad260f5345cf8
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-06-03 13:16:35 +0400

    pack.sh changed to ignore the 'patches' directory.

--- pack.sh
+++ pack.sh
@@ -4,4 +4,4 @@ VERSION=$(grep '^VERSION' YaFotkiUploader.py | sed "s/VERSION = '\(.*\)'/\1/")
 
 cd ..
 
-tar -jcf YaFotkiUploader-$VERSION.tar.bz2 YaFotkiUploader --exclude .git --exclude '*.swp' --exclude '*.pyc' --exclude '*.jpg'
+tar -jcf YaFotkiUploader-$VERSION.tar.bz2 YaFotkiUploader --exclude .git --exclude '*.swp' --exclude '*.pyc' --exclude '*.jpg' --exclude patches
