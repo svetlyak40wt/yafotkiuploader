97e5c1661a4d96659550baee3e71b9eb19639299
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-06-06 16:10:41 +0400

    Added ChangeLog generation during packaging

--- pack.sh
+++ pack.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-VERSION=$(grep '^VERSION' YaFotkiUploader.py | sed "s/VERSION = '\(.*\)'/\1/")
+VERSION=$(grep '^VERSION' YaFotki/__init__.py | sed "s/VERSION = '\(.*\)'/\1/")
+
+[ -e .git ] && git-log --pretty=format:"%ai  %an%n%n  * %s%n" > ChangeLog
 
 cd ..
 
-tar -jcf YaFotkiUploader-$VERSION.tar.bz2 YaFotkiUploader --exclude .git --exclude '*.swp' --exclude '*.pyc' --exclude '*.jpg' --exclude patches
+tar -jcf YaFotkiUploader-$VERSION.tar.bz2 YaFotkiUploader \
+        --exclude .git \
+        --exclude '*.swp' \
+        --exclude '*.pyc' \
+        --exclude '*.jpg' \
+        --exclude patches \
+        --exclude images
