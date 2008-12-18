c94a68eab69a20b380e8922c01afa951c18a19f6
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-12-18 20:43:10 +0300

    Simple setup script for distutils.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'yafotki',
+      version = '0.1.0',
+      description = 'Client library for Yandex.Fotki.',
+      author = 'Alexander Artemenko',
+      author_email = 'art@yandex-team.ru',
+      url = 'http://fotki.yandex.ru/',
+      packages = ['yafotki',],
+      scripts = ['yaploader',],
+     )
+
